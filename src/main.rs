29d6d2c6efbde42cc75d6dad95a2a16b10eeb54c
Nicolas BOULIOL <nbouliol@vente-privee.com>
2019-05-24 20:17:33 +0200

    working

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,6 @@ use elastic;
 fn main() -> Result<(), Box<std::error::Error>> {
     let elastic_infos = elastic::get_indexes();
     println!("{:?}", elastic_infos);
+    elastic::clean_indexes(&elastic_infos?).expect("Something wrong happened");
     Ok(())
 }
